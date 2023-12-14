ffab201bf0c2e6106d7ce5bd90f8cac369793431
Arran Standish <arranstandish@hotmail.com>
2023-12-14 11:27:39 +0200

    Rework logic to instead pull from fhir and delete direct to elastic

--- 1-extract.js
+++ 1-extract.js
@@ -1,27 +1,50 @@
-import { extractPatientIds } from './elastic/index.js';
-import { processMongoData, disconnect as mongoDisconnect } from './mongo/index.js';
-import { openWriteStreams, readElasticPatientIds } from './filesystem/index.js';
+import fs from 'fs';
+import readline from 'readline';
+import { extractPatientIds, deleteResources } from './fhir/index.js';
+import { flushCursor, getCursor } from './filesystem/index.js';
 
 function setupEnv() {
-  if (!process.env.FACILITY_ID) process.env.FACILITY_ID = 'placeholder';
-  if (!process.env.MONGO_URL) process.env.MONGO_URL = 'mongodb://localhost:27017';
+  if (!process.env.FACILITY_ID) process.env.FACILITY_ID = '009a6a861c1b45778c0cbedadefe52a4';
   if (!process.env.ELASTIC_URL) process.env.ELASTIC_URL = 'http://localhost:9201'
   if (!process.env.ELASTIC_PASSWORD) process.env.ELASTIC_PASSWORD = 'dev_password_only';
+  if (!process.env.HAPI_FHIR_URL) process.env.HAPI_FHIR_URL = 'localhost';
+  if (!process.env.HAPI_FHIR_PORT) process.env.HAPI_FHIR_PORT = 3447;
+
 }
+
+// @TODO
+// once processed all patients
+// loop through all patient ids and delete fhir-enrich-report for patient
+// delete patient in hapi-fhir
+// delete organization
 
 async function main() {
   setupEnv();
-  openWriteStreams();
 
   const healthFacilityId = process.env.FACILITY_ID;
-  await extractPatientIds(healthFacilityId);
   
-  const patientIds = await readElasticPatientIds();
-  await processMongoData(patientIds);
+  const cursor = await getCursor();
+  // if we have a cursor we must not extract from elastic (since we already did and are rerunning)
+  if (!cursor) await extractPatientIds(healthFacilityId);
+  
+  let previousCursorFound = cursor === '';
+  const patientIdReader = readline.createInterface({
+    input: fs.createReadStream(`./output/patient-ids.csv`)
+  });
+
+  for await (const patientId of patientIdReader) {
+    if (!previousCursorFound) {
+      if (patientId === cursor) previousCursorFound = true;
+      else continue;
+    }
+
+    await flushCursor(patientId);
+
+    await deleteResources(patientId);
+  }
+
+  await flushCursor();
 }
 
 main()
   .catch(err => console.error(err))
-  .finally(() => {
-    mongoDisconnect();
-  });
--- filesystem/index.js
+++ filesystem/index.js
@@ -1,105 +1,5 @@
 import fs from 'fs';
 import readline from 'readline';
-import fhirTypes from '../fhir/types.js';
-
-let patientStream
-let encounterStream
-let relatedPersonStream
-let medicationDispenseStream
-let serviceRequestStream
-let observationStream
-let careplanStream
-let diagnosticReportStream
-let procedureStream
-let medicationStatementStream
-let questionnaireResponseStream
-let unknownStream
-let malformedStream
-
-export function openWriteStreams() {
-  patientStream = fs.createWriteStream('./output/0-patient.csv');
-  encounterStream = fs.createWriteStream('./output/1-encounter.csv');
-  relatedPersonStream = fs.createWriteStream('./output/2-relatedPerson.csv');
-  medicationDispenseStream = fs.createWriteStream('./output/3-medicationDispense.csv');
-  careplanStream = fs.createWriteStream('./output/4-careplan.csv');
-  observationStream = fs.createWriteStream('./output/5-observation.csv');
-  serviceRequestStream = fs.createWriteStream('./output/6-serviceRequest.csv');
-  diagnosticReportStream = fs.createWriteStream('./output/7-diagnosticReport.csv');
-  procedureStream = fs.createWriteStream('./output/8-procedure.csv');
-  medicationStatementStream = fs.createWriteStream('./output/9-medicationStatement.csv');
-  questionnaireResponseStream = fs.createWriteStream('./output/10-questionnaireResponse.csv');
-  unknownStream = fs.createWriteStream('./output/11-unknown.csv');
-  malformedStream = fs.createWriteStream('./output/12-malformed.csv');
-}
-
-export function writeResourceId(resource, id) {
-  switch (resource) {
-    case fhirTypes.Patient:
-      patientStream.write(`Patient/${id}\n`);
-      break;
-    case fhirTypes.Encounter:
-      encounterStream.write(`Encounter/${id}\n`);
-      break;
-    case fhirTypes.RelatedPerson:
-      relatedPersonStream.write(`RelatedPerson/${id}\n`);
-      break;
-    case fhirTypes.MedicationDispense:
-      medicationDispenseStream.write(`MedicationDispense/${id}\n`);
-      break;
-    case fhirTypes.ServiceRequest:
-      serviceRequestStream.write(`ServiceRequest/${id}\n`);
-      break;
-    case fhirTypes.Observation:
-      observationStream.write(`Observation/${id}\n`);
-      break;
-    case fhirTypes.CarePlan:
-      careplanStream.write(`CarePlan/${id}\n`);
-      break;
-    case fhirTypes.DiagnosticReport:
-      diagnosticReportStream.write(`DiagnosticReport/${id}\n`);
-      break;
-    case fhirTypes.Procedure:
-      procedureStream.write(`Procedure/${id}\n`);
-      break;
-    case fhirTypes.MedicationStatement:
-      medicationStatementStream.write(`MedicationStatement/${id}\n`);
-      break;
-    case fhirTypes.QuestionnaireResponse:
-      questionnaireResponseStream.write(`QuestionnaireResponse/${id}\n`);
-      break;
-    default:
-      unknownStream.write(`${resource}/${id}\n`);
-      break;
-  }
-}
-
-export function writeMalformedResource(transactionId, resource) {
-  malformedStream.write(`Failed to process transaction: ${transactionId}\n`);
-  malformedStream.write(JSON.stringify(resource));
-  malformedStream.write('\n\n');
-}
-
-export function writeSqlConflictResource(resource) {
-  if (!writeSqlConflictResource.writer)
-    writeSqlConflictResource.writer = fs.createWriteStream('./output/sqlconflicts.csv');
-  
-    writeSqlConflictResource.writer.write(`${resource}\n`);
-}
-
-export async function readElasticPatientIds() {
-  return new Promise((resolve, reject) => {
-    fs.readFile('./output/elastic-patients.csv', (err, data) => {
-      if (err) reject(err);
-      const ids = data.toString().split('\n');
-      const idsMap = new Map();
-      for (const id of ids) {
-        if (id !== '') idsMap.set(id, true);
-      }
-
-      resolve(idsMap);
-    })
-  })
-}
 
 export async function processFile(fileName, callback) {
   const reader = readline.createInterface({
@@ -109,4 +9,43 @@ export async function processFile(fileName, callback) {
   for await (const line of reader) {
     await callback(line);
   }
+}
+
+export async function writePatientId(id) {
+  if (!writePatientId.patientIdStream) writePatientId.patientIdStream = fs.createWriteStream('./output/patient-ids.csv');
+  await new Promise((resolve) =>
+    writePatientId.patientIdStream.write(`${id}\n`, resolve)
+  );
+}
+
+export function writePatientResourcesToFile(resources) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./output/patient-resources.json', JSON.stringify(resources), (err) => {
+      if (err) reject(err);
+      else resolve(true);
+    })
+  })
+}
+
+export async function flushCursor(cursor) {
+  await new Promise((resolve, reject) => {
+    fs.writeFile('./output/cursor.dat', cursor, (err) => {
+      if (err) return reject(err);
+      resolve();
+    })
+  })
+}
+
+export function getCursor() {
+  return new Promise((resolve, reject) => {
+    fs.access('./output/cursor.dat', fs.constants.F_OK, (err) => {
+      // file does not exist so no previous cursor
+      if (err) return resolve('');
+
+      fs.readFile('./output/cursor.dat', (err, data) => {
+        if (err) return reject(err);
+        return resolve(data.toString());
+      })
+    })
+  })
 }
